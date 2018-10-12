aa721e7018ea10810cc31aea62ed948056474f90
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-10-12 09:20:25 +0800

    Add files via upload

---
+++ print a sheep.py
@@ -0,0 +1,26 @@
+from time import *
+
+print("*", end='')
+sleep(0.5)
+print('  *')
+sleep(0.5)
+print(' -', end='')
+sleep(0.5)
+
+for i in range(6):
+          print('-', end='')
+          sleep(0.5)
+
+print('\\')
+sleep(0.5)
+print(' |', end='')
+sleep(0.5)
+print(' |', end='')
+sleep(0.5)
+print(' |', end='')
+sleep(0.5)
+print(' |', end='')
+sleep(0.5)
+print(' *')
+sleep(1)
+print('This is a sheep!')
