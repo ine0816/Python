cefec57cb5c9f9c038a687e7628964d534483cf8
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-07 11:42:59 +0800

    Update and rename 泡沫排序法.py to pao mo pai xu fa.py

---
+++ pao mo pai xu fa.py
@@ -0,0 +1,19 @@
+def insertion_sort(list):
+    for index in range (1,len(list)):
+        value = list[index]
+        i = index - 1
+        while i >= 0:
+            if value < list[i]:
+                list[i+1]=list[i] #shift number in slot i right to slot i+1
+                list[i]=value #shift value left into slot i
+                i = i - 1
+            else:
+                break
+                
+###
+泡沫排序法
+a=[4,5,6,8,2,1,9]
+print(a)
+insertion_sort(a)
+print(a)
+###
