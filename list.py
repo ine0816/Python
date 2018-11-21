5924f4b80f40c4b025f61497ab97cafcf259d929
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-21 10:27:04 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+list1 = ['Google', 'Runoob', 1997, 2000];
+list2 = [1, 2, 3, 4, 5 ];
+list3 = ["a", "b", "c", "d"];
+print(list1)
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
+print("list3[-1]: ",list3[-1])
