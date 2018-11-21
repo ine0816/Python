6104b0cfeb328df1bf33118f074ddcbb89896158
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-21 10:40:59 +0800

    Update list.py

--- list.py
+++ list.py
@@ -1,12 +1,19 @@
 list1 = ['Google', 'Runoob', 1997, 2000]
 list2 = [1, 2, 3, 4, 5 ]
 list3 = ["a", "b", "c", "d"]
+
 print(list1)
+print(list2)
+print(list3,'\n')
+
 print ("list1[0]: ", list1[0])
-print ("list2[1:5]: ", list2[1:5])
+
+print("list2[1:5]: ", list2[1:5])
+
 print("Print list3:",list3)
-print("Print list3 last number(list3[-1]): ",list3[-1])
+
+print("Print list3 last number(list3[-1]): ",list3[-1],'\n')
 del list3[-1]
-print("Del list3 last number",list3)
+print("Del list3 last number",list3,'\n')
 list1.append('Baidu')
-print ("list1 增加Baidu 更新后的列表 : ", list1)
+print("list1 增加Baidu 更新后的列表 : ", list1)
