3b355f92c652418e5753dedd17c1498211444c02
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-21 10:36:12 +0800

    Update list.py

--- list.py
+++ list.py
@@ -4,4 +4,9 @@ list3 = ["a", "b", "c", "d"];
 print(list1)
 print ("list1[0]: ", list1[0])
 print ("list2[1:5]: ", list2[1:5])
-print("list3[-1]: ",list3[-1])
+print("Print list3:",list3)
+print("Print list3 last number(list3[-1]): ",list3[-1])
+del list3[-1]
+print("Del list3 last number",list3)
+list1.append('Baidu')
+print ("list1 增加Baidu 更新后的列表 : ", list1)
