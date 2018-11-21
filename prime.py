29bed36cdfcff5dbec26e878be7c02a718e2f826
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-21 10:15:13 +0800

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,9 @@
+z = int(input("A number:"))
+for n in range(2, z):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
