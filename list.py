17a875d49cd66b1aa64098db7906bfa04485c479
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-21 10:42:06 +0800

    Update list.py

--- list.py
+++ list.py
@@ -2,9 +2,9 @@ list1 = ['Google', 'Runoob', 1997, 2000]
 list2 = [1, 2, 3, 4, 5 ]
 list3 = ["a", "b", "c", "d"]
 
-print(list1)
-print(list2)
-print(list3,'\n')
+print('list1:',list1)
+print('list2:',list2)
+print('list3:',list3,'\n')
 
 print ("list1[0]: ", list1[0])
 
