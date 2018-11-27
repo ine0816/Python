c1a3f10f9d90c60db388243f15ab76e8f4ba724f
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-11-27 09:31:00 +0800

    Create append list.py

---
+++ append list.py
@@ -0,0 +1,11 @@
+a=[]
+print("This is fist LIST a:",a)
+
+for i in range(1,20,2):
+    a.append(i)
+    print(i)
+
+print("This is a last LIST a:",'\n',a)
+print(sum(a))
+print(max(a))
+print(min(a))
