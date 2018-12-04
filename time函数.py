4150932f0aed9d3414ffd9b792f0d72bc3685d89
Kinz GitHub <41033200+ine0816@users.noreply.github.com>
2018-12-04 09:02:02 +0800

    Create time函数.py

---
+++ time函数.py
@@ -0,0 +1,24 @@
+import time
+
+#获取可读的时间模式的函数是asctime()
+localtime = time.asctime( time.localtime(time.time()) ) 
+print ("本地时间为 :", localtime)
+
+# 格式化成2016-03-20 11:45:39形式
+print (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+# 格式化成Sat Mar 28 22:24:24 2016形式
+print (time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()))
+  
+# 将格式字符串转换为时间戳
+a = "Sat Mar 28 22:24:24 2016"
+print (time.mktime(time.strptime(a,"%a %b %d %H:%M:%S %Y")))
+
+print('\n\n\n')
+
+#Calendar模块有很广泛的方法用来处理年历和月历，例如打印某月的月历
+import calendar
+
+cal = calendar.month(2018, 12)
+print ("2018年12月份的日历:")
+print (cal)
